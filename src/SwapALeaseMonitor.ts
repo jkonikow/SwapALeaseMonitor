018d334e36925950218d67e390104b7456ee3ff9
Jkon1513 <jkon1513@gmail.com>
2022-06-21 16:29:50 -0700

    [temp] define main class

--- src/SwapALeaseMonitor.ts
+++ src/SwapALeaseMonitor.ts
@@ -1,5 +1,29 @@
+import SwapALeaseClient from "@jkon1513/swap-a-lease-client/src/client/SwapALeaseClient";
+import GetListingsRequest from "@jkon1513/swap-a-lease-client/src/client/GetListingsRequest";
+import GetListingsResponse from "@jkon1513/swap-a-lease-client/src/client/GetListingsResponse"; 
+
 class SwapALeaseMonitor {
+    private readonly client: SwapALeaseClient; 
+
+    public constructor(client: SwapALeaseClient) {
+        this.client = client;
+    }
+
     public run(): void {
-        console.log("monitoring...");
+        this.monitor();
+    }
+
+    private monitor(): void {
+        console.log("running monitor...");
+        const request: GetListingsRequest = GetListingsRequest.builder()
+                                                .withMaxDistanceFromZip('100')
+                                                .withMinMilesPerMonth('1000')
+                                                .withMaxMonthsRemaining('24')
+                                                .withMaxPricePerMonth('650')
+                                                .withZip('07675')
+                                                .build();
+        this.client.getListings(request)
+        .then((response: GetListingsResponse) => console.log(response.getListings()))
+        .catch(e => console.error(e));
     }
 }
