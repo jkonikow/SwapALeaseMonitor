daa65adc4a8a0e8f06a9f58fdd8860de3b6808eb
Jkon1513 <jkon1513@gmail.com>
2022-06-24 23:48:35 -0700

    Add environment module

---
+++ src/environment/environment_module.ts
@@ -0,0 +1,16 @@
+const BUCKET_NAME_KEY: string  = "LISTINGS_S3_BUCKET_NAME";
+const REGION_KEY: string = "AWS_REGION";
+const OBJECT_KEY: string = "LISTINGS_OBJECT";
+
+const environment: Record<string, string> = {};
+[BUCKET_NAME_KEY, REGION_KEY, OBJECT_KEY].forEach(key => {
+    if (!process.env[key]) {
+        throw new Error(`Missing environment variable for ${key}`);
+    }
+});
+
+export const {
+    [BUCKET_NAME_KEY]: LISTINGS_S3_BUCKET_NAME,
+    [REGION_KEY]: AWS_REGION,
+    [OBJECT_KEY]: LISTINGS_OBJECT
+} = environment;
